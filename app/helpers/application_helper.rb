d3d3b56b18bd61162138ba99fd9f1fcb9065bcc4
Jitang Zheng <jitang.zheng@gmail.com>
2016-10-03 08:50:08 -0700

    navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  def top_links
+    links = Setting.get_site_info :top_links
+    if links.nil?
+      return {}
+    end
+    links.split("\r\n").reduce({}) do |h, l|
+      args = l.split(' ')
+      if args.size == 2
+        h[args.first.to_sym] = args.last
+      end
+      h
+    end
+  end
+
 end
